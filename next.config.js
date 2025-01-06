4c620b663ab3d19d0d773a86ab20262331014cd3
Mateus Pires <mateus.dev.ti@gmail.com>
2025-01-06 15:15:11 -0300

    ajustes gtm final

--- next.config.js
+++ next.config.js
@@ -2,45 +2,50 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-// Atualização na Content Security Policy para permitir GTM
+// Atualização na Content Security Policy para permitir GTM e Simple Analytics
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://www.googletagmanager.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://www.googletagmanager.com https://scripts.simpleanalyticscdn.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app https://www.googletagmanager.com;
+  frame-src giscus.app https://www.googletagmanager.com https://scripts.simpleanalyticscdn.com;
 `
 
 const securityHeaders = [
+  // Content Security Policy
   {
-    // Content Security Policy
     key: 'Content-Security-Policy',
     value: ContentSecurityPolicy.replace(/\n/g, ''),
   },
-  // Outras políticas de segurança...
+  // Referrer Policy
   {
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin',
   },
+  // X-Frame-Options
   {
     key: 'X-Frame-Options',
     value: 'DENY',
   },
+  // X-Content-Type-Options
   {
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
+  // X-DNS-Prefetch-Control
   {
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
   },
+  // Strict-Transport-Security
   {
     key: 'Strict-Transport-Security',
     value: 'max-age=31536000; includeSubDomains',
   },
+  // Permissions Policy
   {
     key: 'Permissions-Policy',
     value: 'camera=(), microphone=(), geolocation=()',
