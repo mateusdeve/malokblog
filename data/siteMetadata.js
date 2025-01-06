c13820dc3eedb1db16c4756100f6a15814e1db99
Mateus Pires <mateus.dev.ti@gmail.com>
2025-01-06 15:40:22 -0300

    merge

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'Seu guia descontraído para o mundo tech',
   language: 'pt-br',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://quelespiqueblog.vercel.app/',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.jpeg',
