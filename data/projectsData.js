914c52f015d851a82bc1205c65c1d2c4b7f92197
Mateus Pires <mateus.dev.ti@gmail.com>
2025-01-07 15:51:43 -0300

    adicionando projetos ao blog

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,20 +1,40 @@
 const projectsData = [
-  // {
-  //   title: 'A Search Engine',
-  //   description: `What if you could look up any information in the world? Webpages, images, videos
-  //   and more. Google has many features to help you find exactly what you're looking
-  //   for.`,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
-  // {
-  //   title: 'The Time Machine',
-  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-  //   to the desired date and press "Go". No more worrying about lost keys or
-  //   forgotten headphones with this simple yet affordable solution.`,
-  //   imgSrc: '/static/images/time-machine.jpg',
-  //   href: '/blog/the-time-machine',
-  // },
+  {
+    title: 'Site de Venda para o Evento "O Novo Mundo"',
+    description: `Desenvolvido utilizando Next.js e Tailwind CSS, este site foi projetado para ser uma página de vendas altamente eficaz e visualmente atraente para o evento "O Novo Mundo" – o maior evento da América Latina voltado para pessoas comuns que desejam criar um negócio na internet.`,
+    imgSrc: '/static/images/projects/novo-mundo.jpg',
+    href: 'https://vendatodosantodia.com.br/o-novo-mundo/',
+  },
+  {
+    title: 'Site Institucional para "Cooperação Ambiental"',
+    description: `Desenvolvido utilizando HTML, CSS, JavaScript, Joomla, PHP e Bootstrap, este site serve como uma plataforma informativa e interativa para a iniciativa "Cooperação Ambiental", destacando o papel fundamental do cooperativismo no cuidado das pessoas, das comunidades e do meio ambiente.`,
+    imgSrc: '/static/images/projects/coop-amb.jpg',
+    href: 'https://cooperacaoambiental.coop.br/pt/',
+  },
+  {
+    title: 'Blog "Resultados VTSD"',
+    description: `Desenvolvido utilizando Next.js, TypeScript e Tailwind CSS no front-end, e NestJS com MySQL no back-end, este blog serve como uma plataforma dedicada a compartilhar os resultados e histórias de sucesso dos alunos do VTSD`,
+    imgSrc: '/static/images/projects/resultados.jpg',
+    href: 'https://resultado.vtsd.com.br/',
+  },
+  {
+    title: 'Site "Cooperativismo Brasileiro 2024"',
+    description: `Desenvolvido utilizando HTML, CSS, JavaScript, Joomla, PHP e Bootstrap, este site serve como uma plataforma informativa e interativa dedicada a apresentar dados e insights sobre o Cooperativismo Brasileiro em 2024. Através da integração de dados provenientes de arquivos CSV, o site oferece informações atualizadas e detalhadas sobre diversas cooperativas, seus desempenhos, setores de atuação e impacto socioeconômico.`,
+    imgSrc: '/static/images/projects/coop-anu.jpg',
+    href: 'https://resultado.vtsd.com.br/',
+  },
+  {
+    title: 'Site de Vendas da Mentoria "Fluxo"',
+    description: `Desenvolvido utilizando Next.js, TypeScript e Tailwind CSS, este site é uma página de vendas altamente eficaz e visualmente impactante para a Mentoria Fluxo. Através de um design moderno e funcionalidades interativas, o site visa converter visitantes em participantes da mentoria, oferecendo informações detalhadas e incentivos atrativos.`,
+    imgSrc: '/static/images/projects/mentoria-flx.webp',
+    href: 'https://resultado.vtsd.com.br/',
+  },
+  {
+    title: 'Sistema de Iscas Digitais Interno VTSD',
+    description: `Desenvolvido utilizando Next.js, Tailwind CSS, Amazon S3, NestJS e MySQL, este sistema de iscas digitais é uma solução robusta e eficiente voltada para uso interno. Ele permite a criação rápida de páginas de captura de leads em menos de 5 minutos, facilitando estratégias de marketing digital e geração de leads para a organização.`,
+    imgSrc: '/static/images/projects/iscas-sistem.jpg',
+    href: 'https://iscas.vtsd.com.br/',
+  },
 ]
 
 export default projectsData
